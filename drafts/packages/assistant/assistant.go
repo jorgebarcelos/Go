daf192a0e2da63eb95b1b7d0cdb5411875fb9b19
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-06-18 18:07:15 -0300

    first commit

---
+++ drafts/packages/assistant/assistant.go
@@ -0,0 +1,7 @@
+package assistant
+
+
+func SumNumbers(n1, n2 float32) float32 {
+	println(n1 + n2)
+	return n1 + n2
+}
---
+++ drafts/packages/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"module/assistant"
+)
+
+func main() {
+	assistant.SumNumbers(3.5, 8.37)
+}
